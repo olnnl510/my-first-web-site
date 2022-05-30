ff9fc16db665f793ca3af959df834669c42e52c3
olnnl510 <101864135+olnnl510@users.noreply.github.com>
2022-05-30 23:24:19 +0900

    Add files via upload

--- mfws-php/function2.php
+++ mfws-php/function2.php
@@ -10,7 +10,7 @@
     <h1>Function</h1>
     <h2>Basic</h2>
     <?php
-        // 함수 정의
+        // 함수 정의 (출력기능 내장 : 쓸모 적음)
         function basic() {
             print("Lorem ipsum dolor1<br>");
             print("Lorem ipsum dolor2<br>");
@@ -24,12 +24,22 @@
     <h2>parameter &amp; argument</h2>
     <?php
     function sum($left, $right) { // 입력값 2개
-        print($left+$right);
+        print($left + $right);
         print("<br>");
-
+    }
         sum(2,4); // left라는 변수의 값을 2, right라는 변수의 값을 4로 설정. (중괄호 안에서 사용!) => 입력값을 뭘 주느냐에 따라서 다르게 동작
         sum(4,6);
+    
+    ?>
+    <h2>return</h2>
+    <?php
+    function sum2($left, $right) {
+        return $left+$right;
+        echo 323234; // return을 만나는 순간 함수는 종료 돼버림. 밑에 무시됨.
     }
+        print(sum2(2,4)); // 내부적으로 계산한 결과 리턴 : 쓸모 많음(다양한 용도로 쓸수 있음)
+        print("<br>");
+        file_put_contents('result.txt', sum(2,4));
     ?>
 </body>
 </html>
--- mfws-php/index copy.php
+++ mfws-php/index copy.php
@@ -2,7 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>
+      <?php
+        if(isset($_GET['id'])) {
+          echo $_GET['id'];
+        } else {
+          echo "Welcome";
+        }
+      ?></title>
 </head>
 <body>
     <h1><a href="index.php">WEB</a><h1>
@@ -28,16 +35,19 @@
         </ol>
         <h2>
           <?php
-          if(isset($_GET['id'])){
-            echo $_GET['id'];}
-          else {echo "Welcome";}
+          if(isset($_GET['id'])) {
+            echo $_GET['id'];
+          } else {
+            echo "Welcome";
+          }
           ?>
         </h2>
         <?php
-        if(isset($_GET['id'])){
+        if(isset($_GET['id'])) {
           echo file_get_contents("data/".$_GET['id']); // echo data/id 값에 해당하는 파일의 내용
+        } else {
+          echo "Hello, PHP";
         }
-        else {echo "Hello, PHP";}
         ?>
 </body>
 </html>
