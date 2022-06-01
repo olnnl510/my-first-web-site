599d74a815e8a56375c9d176b13f1e0b5e57b3bd
olnnl510 <101864135+olnnl510@users.noreply.github.com>
2022-06-02 00:33:49 +0900

    Add files via upload

---
+++ mfws-php/form.php
@@ -0,0 +1,7 @@
+<?php
+file_put_contents('data/'.$_POST['title'], $_POST['description']);
+// file_put_contents('data/'.$_GET['title'], $_GET['description']);
+
+// echo "<p>title : " . $_GET['title'] . "</p>";
+// echo "<p>description : " . $_GET['description'] . "</p>";
+?>
